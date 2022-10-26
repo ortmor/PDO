a2360551bc606c966ff606a9418e410f248f53ed
unknown <rekhilelias2014@gmail.com>
2022-10-26 19:57:31 +0530

    u

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,10 +1,23 @@
 import React, { useState, useRef } from "react";
 import Video from "../Media/Videos/video.mp4";
 import Card from "../Media/Images/Gallery/A.jpg";
+import Card1 from "../Media/Images/Gallery/B.jpg";
+import Card2 from "../Media/Images/Gallery/C.jpg";
+import Card3 from "../Media/Images/Gallery/D.jpg";
+import Card4 from "../Media/Images/Gallery/E.jpg";
+import Card5 from "../Media/Images/Gallery/F.jpg";
+import Card6 from "../Media/Images/Gallery/G.jpg";
+import Card7 from "../Media/Images/Gallery/H.jpg";
+import Card8 from "../Media/Images/Gallery/I.jpg";
+import Card9 from "../Media/Images/Gallery/J.jpg";
+import Card10 from "../Media/Images/Gallery/K.jpg";
+import Card11 from "../Media/Images/Gallery/L.jpg";
+import Card12 from "../Media/Images/Gallery/M.jpg";
+import Card13 from "../Media/Images/Gallery/N.jpg";
+
 import { EffectCreative, Autoplay, FreeMode, Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
-
 
 function Gallery() {
   const [toggleplay, settoggleplay] = useState();
@@ -52,8 +65,42 @@ function Gallery() {
           <img src={Card}></img>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Card}></img>
+          <img src={Card1}></img>
         </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card2}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card3}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card4}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card5}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card6}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card7}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card8}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card9}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card10}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card11}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Card12}></img>
+        </SwiperSlide>
+
         <SwiperSlide>
           <video
             controls={false}

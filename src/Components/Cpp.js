b786d9d049d9749e25422cce148e0f72bce695df
unknown <rekhilelias2014@gmail.com>
2022-10-28 09:33:04 +0530

    updated final version 01

--- src/Components/Cpp.js
+++ src/Components/Cpp.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "../Styles/home.scss";
-import BgVideo from "../Media/Videos/02_bg.mp4";
+import BgVideo from "../Media/Videos/Cpp.mp4";
 import Footer from "./Footer";
 
 function Cpp() {
   return (
     <div className="home-container">
-      <div className="inner-carousel-heading">
-        <div className="inner-carousel-head">
-          <h1>Central Processing Plant (CPP)</h1>
-        </div>
-      </div>
-
       <div className="carousel-container">
         <div className="container-carousel">
           <video
--- src/Components/Noor.js
+++ src/Components/Noor.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "../Styles/home.scss";
-import BgVideo from "../Media/Videos/02_bg.mp4";
+import BgVideo from "../Media/Videos/Noor.mp4";
 import Footer from "./FooterG";
 
 function Noor() {
   return (
     <div className="home-container">
-      <div className="inner-carousel-heading">
-        <div className="inner-carousel-head">
-          <h1>Project Noor</h1>
-        </div>
-      </div>
-
       <div className="carousel-container">
         <div className="container-carousel">
           <video
